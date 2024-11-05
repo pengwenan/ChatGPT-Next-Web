3c4798ce441b3aa650800d76576e2894215c5afa
pengwenan <113676489+pengwenan@users.noreply.github.com>
2024-11-05 19:03:43 +0800

    Update page.tsx
    添加飞书调试

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 
 import { getServerSideConfig } from "./config/server";
-
+import Script from 'next/script';
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
   return (
     <>
+     <Script
+        src="https://sf1-scmcdn-cn.feishucdn.com/obj/feishu-static/op/fe/devtools_frontend/remote-debug-0.0.1-alpha.6.js"
+        strategy="beforeInteractive" // 你可以根据需要选择加载策略
+      />
       <Home />
       {serverConfig?.isVercel && (
         <>
